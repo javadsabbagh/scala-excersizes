e648aede901f2a18d660d1673db72460ba4b6066
Javad Sabbagh <javad.sabbaq@gmail.com>
2021-01-31 16:05:57 +0330

    Add EnumTest

--- src/main/scala/EnumTest.scala
+++ src/main/scala/EnumTest.scala
@@ -1,20 +1,34 @@
 
 package com.kiatack.practice
 
+sealed trait Test
+// case classes take parameters
+final case class MM(val code: String) extends Test
+
 sealed trait Nationality
-final case object Iranian extends Nationality
-final case object Foreign extends Nationality
+// case object does not take parameters
+final case object Iranian extends Nationality {
+  val code = 1
+}
+final case object Foreign extends Nationality {
+  val code = 2
+}
 
 sealed trait CustomerKind
-final case object Real extends CustomerKind
-final case object Legal extends CustomerKind
+final case object Real extends CustomerKind {
+  val code = 1
+}
+final case object Legal extends CustomerKind {
+  val code = 2
+}
 
 object EnumTest {
+  val t = MM("Hiii")
   val m: Nationality = Iranian
   val n: CustomerKind =  Real
 
   (m, n) match {
-    case (Iranian, Real) => println("Real-Iranian")
+    case (Iranian, Real) => println(s"Real-Iranian: ${Iranian.code}")
     case (Iranian, Legal) => println("Legal-Iranian")
     case (Foreign, Real) => println("Real-Foreign")
     case (Foreign, Legal) => println("Legal-Foreign")
